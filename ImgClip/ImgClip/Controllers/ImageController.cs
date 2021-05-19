818dcd4e25998217731eac36989dab4e4a7d788b
琢磨先生 <308360781@qq.com>
2021-05-20 01:04:48 +0800

    create project

---
+++ ImgClip/ImgClip/Controllers/ImageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImgClip.Controllers
+{
+
+    public class ImageController : Controller
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Route("/")]
+        public IActionResult DisPlay(string remote_url)
+        {
+
+            return Ok();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
